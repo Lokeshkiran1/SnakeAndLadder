3a5ae26cb1e1d4669d8e09ccb979197fb1d8ddfa
LokeshKiran <lokeshkskiran@gmail.com>
2022-08-30 19:51:42 +0530

    [Lokesh]Added:UC-2 Snake And Ladder

--- Program.cs
+++ Program.cs
@@ -11,6 +11,21 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Snake and Ladder Game");
+            Console.WriteLine("Choose option\n1.Check position of player\n2.Rolling of Dice");
+            int choice=int.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    UCOneSnakeAndLadder.ucOne();
+                    break;
+                    case 2:
+                    UCTwoSnakeAndLadder.ucTwo();
+                    break;
+                    default:
+                    Console.WriteLine("enter valid choice");
+                    break;
+
+            }
         }
     }
 }
--- UCTwoSnakeAndLadder.cs
+++ UCTwoSnakeAndLadder.cs
@@ -8,5 +8,12 @@ namespace SnakeAndLadder
 {
     internal class UCTwoSnakeAndLadder
     {
+        public static void ucTwo()
+        {
+            int positionOfPlayer = 0;
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            Console.WriteLine("Position of player at initial is : " + positionOfPlayer +", and rolled a die : " + dice);
+        }
     }
 }
